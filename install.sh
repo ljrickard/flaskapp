b7e3257d868d0d7eeb97b0463cca7513f8226497
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-04 17:27:14 +0000

    updating deployment scripts 3

--- install.sh
+++ install.sh
@@ -2,20 +2,29 @@
 #sudo yum install python36
 #yum install python-pip
 
+#/home/ec2-user
+
+#systems root directory
+#opt/codedeploy-agent/
+
+
+
+
 #test -d '/etc/init.d' || mkdir -p '/etc/init.d'
 pwd
-#mkdir deploy
-cd deploy
-wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.8.tar.gz
-tar xzvf virtualenv-1.8.tar.gz
+mkdir ~/deploy
+cd ~/deploy
+
+#wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.8.tar.gz
+#tar xzvf virtualenv-1.8.tar.gz
 
 #python virtualenv-1.8/virtualenv.py $HOME/env
-python virtualenv-1.8/virtualenv.py ./virtualenv
+#python virtualenv-1.8/virtualenv.py ./virtualenv
 
-virtualenv pyenv
+#virtualenv pyenv
 
-. ./pyenv/bin/activate
+#. ./pyenv/bin/activate
 
 
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
