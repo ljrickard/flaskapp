5273391756361317a5d501175abf3dd321b33569
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-04 16:50:13 +0000

    updating deployment scripts

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-sudo yum install python36
-yum install python-pip
+#sudo yum install python36
+#yum install python-pip
 
 #test -d '/etc/init.d' || mkdir -p '/etc/init.d'
 
-mkdir deploy
+#mkdir deploy
 cd deploy
 wget https://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.8.tar.gz
 tar xzvf virtualenv-1.8.tar.gz
 
-python virtualenv-1.8/virtualenv.py $HOME/env
+#python virtualenv-1.8/virtualenv.py $HOME/env
 python virtualenv-1.8/virtualenv.py ./virtualenv
 
 virtualenv pyenv
