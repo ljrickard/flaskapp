ea55b1155fa3f8f1d9441ec6c2732dccf3e7a1c7
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-05 14:59:14 +0000

    updating deployment scripts 28

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -11,14 +11,14 @@ echo
 
 echo "Stopping app as `whoami`"
 
-. ./pyenv/bin/activate
+#. ./pyenv/bin/activate
 
-pid=$(cat gunicorn.pid)
+#pid=$(cat gunicorn.pid)
 
-if [ -z "$pid" ]
-then
-      echo "No pid found. No application to stop"
-else
-      echo "Sending TERM signal to $pid"
-      exec kill -s TERM $pid
-fi
+#if [ -z "$pid" ]
+#then
+#      echo "No pid found. No application to stop"
+#else
+#      echo "Sending TERM signal to $pid"
+#      exec kill -s TERM $pid
+#fi
