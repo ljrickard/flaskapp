876e34addf4805082e7fab2e8315eda062f5b6d6
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-13 20:32:14 +0000

    updating deployment scripts 61

--- clean.sh
+++ clean.sh
@@ -3,9 +3,11 @@ VIRTUAL_ENV_DIRECTORY='/home/ec2-user/deploy/virtual_env'
 PY_CACHE_DIRECTORY='/home/ec2-user/deploy/__pycache__'
 
 if [ -d $DIRECTORY ]; then
-  rm -rf $DIRECTORY
+  	echo 'removing virtual_env'
+	rm -rf $DIRECTORY
 fi
 
 if [ -d $PY_CACHE_DIRECTORY ]; then
-  rm -rf $PY_CACHE_DIRECTORY
+	echo 'removing __pycache__'
+  	rm -rf $PY_CACHE_DIRECTORY
 fi
