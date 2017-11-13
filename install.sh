2826f176cd3b975d206777b8f0d637e416a3e20d
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-13 19:34:07 +0000

    updating deployment scripts 53

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ tar xzvf virtualenv-15.1.0.tar.gz
 python36 virtualenv-15.1.0/virtualenv.py /home/ec2-user/deploy/virtual_env
 #. /home/ec2-user/deploy/virtual_env/bin/activate
 #pip install -r /home/ec2-user/deploy/requirements.txt
-
+echo pwd
+echo ls -la
 /home/ec2-user/deploy/virtual_env/bin/pip install -r requirements.txt
