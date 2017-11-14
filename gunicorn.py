b3a886594d841c83bfcb9a4b537c8bca993b79a8
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-14 19:49:13 +0000

    updating deployment scripts 70

--- gunicorn.py
+++ gunicorn.py
@@ -8,5 +8,4 @@ user = None
 errorlog = '-'
 loglevel = 'info'
 accesslog = '-'
-pidfile = GFILE
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #exec /home/ec2-user/deploy/virtual_env/bin/gunicorn -p GFILE -c /home/ec2-user/deploy/gunicorn.py /home/ec2-user/deploy/app:app
-exec /home/ec2-user/deploy/virtual_env/bin/gunicorn -c /home/ec2-user/deploy/gunicorn.py /home/ec2-user/deploy/app:app
+exec /home/ec2-user/deploy/virtual_env/bin/gunicorn -p GFILE -c /home/ec2-user/deploy/gunicorn.py /home/ec2-user/deploy/app:app
      
 #set -e
 
