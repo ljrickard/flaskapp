55e3849c2a32a69aefc7718429da5fb84110fa6f
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-19 15:26:50 +0000

    updating deployment scripts 110

--- stop.sh
+++ stop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -euo pipefail
-VALUE=${"/home/ec2-user/deploy/GFILE":-}
-if [ ! -f VALUE ];
+set -u pipefail
+pid=$(cat /home/ec2-user/deploy/GFILE)
+
+if [ -z "$pid" ]
 then
-	echo "No pid found. No application to stop"
+      echo "No pid found. No application to stop"
 else
-	pid=$(cat /home/ec2-user/deploy/GFILE)
-	echo "Sending TERM signal to $pid"
-	exec kill -s TERM $pid
+      echo "Sending TERM signal to $pid"
+      exec kill -s TERM $pid
 fi
