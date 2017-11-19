051e765ac0d24ad6efd74412f497fb9955ed7710
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-19 11:50:23 +0000

    updating deployment scripts 94

--- app.py
+++ app.py
@@ -8,32 +8,30 @@ from time import sleep
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-# with app.app_context():
-#    app.config.from_object(
-#        'config.{0}'.format(
-#            os.getenv(
-#                'FLASK_CONFIGURATION',
-#                'DevelopmentConfig')))
+    with app.app_context():
+        app.config.from_object(
+            'config.{0}'.format(
+                os.getenv(
+                    'FLASK_CONFIGURATION',
+                    'DevelopmentConfig')))
 
 
-# celery = Celery(app.name, backend=app.config['CELERY_RESULT_BACKEND'], broker = app.config['CELERY_BROKER_URL'])
-# celery.conf.update(app.config)
+celery = Celery(
+    app.name,
+    backend=app.config['CELERY_RESULT_BACKEND'],
+    broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 
 @app.route('/', methods=['POST'])
 def hello_world():
-    #result = do_something_async.delay(30)
+    result = do_something_async.delay(30)
     logger.info(str(result))
     return str(result)
 
-#@celery.task
 
-
+@celery.task
 def do_something_async(t):
     logger.info('starting to do something for {0}'.format(t))
     sleep(t)
     logger.info('finished doing something for {0}'.format(t))
-
-
-if __name__ == "__main__":
-    app.run()
--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-#exec /home/ec2-user/deploy/virtual_env/bin/gunicorn -p GFILE -c /home/ec2-user/deploy/gunicorn.py /home/ec2-user/deploy/app:app
-
-
 /home/ec2-user/deploy/virtual_env/bin/gunicorn --chdir /home/ec2-user/deploy -p /home/ec2-user/deploy/GFILE -c /home/ec2-user/deploy/gunicorn.py app:app
 
 #set -e
