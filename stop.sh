7086948dc2f14b2b21e4bad447acd241f2ed91e9
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-19 15:35:05 +0000

    updating deployment scripts 112

--- stop.sh
+++ stop.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 pid=$(cat /home/ec2-user/deploy/GFILE)
+
 if [ -z "$pid" ]
 then
-	echo "No pid found. No application to stop"
+      echo "No pid found. No application to stop"
 else
-  	echo "Sending TERM signal to $pid"
-	exec kill -s TERM $pid
+      echo "Sending TERM signal to $pid"
+      exec kill -s TERM $pid
 fi
