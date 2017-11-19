79eceab6c80f006b5964e4bbfa437fad5afcb8a0
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-19 13:19:22 +0000

    updating deployment scripts 99

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-if curl -sL -w "%{http_code}\\n" "localhost:8000/status" -o /dev/null
-	then 
-		echo "Endpoint point running successfully"
-	else
-		echo "Endpoint failed to start"
-		exit 1
-fi
+#if curl -sL -w "%{http_code}" "localhost:8000/status" -o /dev/null
+	#then 
+	#	echo "Endpoint point running successfully"
+	#else
+	#	echo "Endpoint failed to start"
+	#	exit 1
+#fi
