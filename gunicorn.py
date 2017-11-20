af9c130251f5be6eb9174b1043daeb88690631dd
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-20 17:47:56 +0000

    updating deployment scripts 121

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 bind = "127.0.0.1:8000"
-workers = 4 #multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'sync'
 worker_connections = 1000
 timeout = 30
