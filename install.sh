096bcf918751919249f61e1c28d35e4d7f4ed326
Luke Rickard <luke.x.rickard@gmail.com>
2017-11-20 10:39:51 +0000

    updating deployment scripts 117

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 yum -y install python36
 wget -O /home/ec2-user/deploy/virtualenv-15.1.0.tar.gz https://github.com/pypa/virtualenv/archive/15.1.0.tar.gz
-tar xzvf virtualenv-15.1.0.tar.gz /home/ec2-user/deploy/
+tar xzvf virtualenv-15.1.0.tar.gz /home/ec2-user/deploy/virtualenv-15.1.0.tar.gz
 python36 /home/ec2-user/deploy/virtualenv-15.1.0/virtualenv.py /home/ec2-user/deploy/virtual_env
 /home/ec2-user/deploy/virtual_env/bin/pip install -r /home/ec2-user/deploy/requirements.txt
