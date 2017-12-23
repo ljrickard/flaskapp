7d27c93dd207fa2466098554d11013d0aec81e41
Luke Rickard <luke.x.rickard@gmail.com>
2017-12-23 14:29:36 +0000

    updating deployment scripts 143

--- stop.sh
+++ stop.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -euo pipefail
+
+/home/ec2-user/deploy/virtual_env/bin/celery multi stopwait w1 \
+	--pidfile="/home/ec2-user/deploy/w1.pid"
+/home/ec2-user/deploy/virtual_env/bin/celery multi stopwait w2 \
+	--pidfile="/home/ec2-user/deploy/w2.pid"
+/home/ec2-user/deploy/virtual_env/bin/celery multi stopwait w3 \
+	--pidfile="/home/ec2-user/deploy/w3.pid"
+/home/ec2-user/deploy/virtual_env/bin/celery multi stopwait w4 \
+	--pidfile="/home/ec2-user/deploy/w4.pid"
+/home/ec2-user/deploy/virtual_env/bin/celery multi stopwait w5 \
+ 	--pidfile="/home/ec2-user/deploy/w5.pid"
+/home/ec2-user/deploy/virtual_env/bin/celery multi stopwait w6 \
+	--pidfile="/home/ec2-user/deploy/w6.pid"
+
 GFILE="/home/ec2-user/deploy/gunicorn.pid"
 
 if [ -e $GFILE ]; then
