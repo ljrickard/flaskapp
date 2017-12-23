bba703f72db46fb86e01473d12c0db7579ebf365
Luke Rickard <luke.x.rickard@gmail.com>
2017-12-23 14:04:02 +0000

    updating deployment scripts 137

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 /home/ec2-user/deploy/virtual_env/bin/gunicorn --chdir /home/ec2-user/deploy -p /home/ec2-user/deploy/GFILE -c /home/ec2-user/deploy/gunicorn.py app:app
 
 
-/home/ec2-user/deploy/virtual_env/bin/celery multi start worker1 \
+/home/ec2-user/deploy/virtual_env/bin/celery multi start c1 c2 c3 c4 \
     -A app.celery \
     --pidfile="/home/ec2-user/deploy/C_FILE.pid" \
-    --logfile="/home/ec2-user/deploy/logs/celery/%n%I.log"
+    --logfile="/home/ec2-user/deploy/logs/celery/%n%I.log" \
     --workdir="/home/ec2-user/deploy"
 
