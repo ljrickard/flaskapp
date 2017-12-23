927eff27d43a327b9b2129201f236d10e98ae5cd
Luke Rickard <luke.x.rickard@gmail.com>
2017-12-23 14:21:20 +0000

    updating deployment scripts 142

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 
 /home/ec2-user/deploy/virtual_env/bin/celery multi start w1 w2 w3 w4 w5 w6 \
     --app=app.celery \
-    --pidfile="/home/ec2-user/deploy/c_%.pid" \
+    --pidfile="/home/ec2-user/deploy/%n.pid" \
     --logfile="/home/ec2-user/deploy/logs/celery/%n%I.log" \
     --workdir="/home/ec2-user/deploy"
 
