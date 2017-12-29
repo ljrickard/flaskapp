9f133b5f4b7d2cb13275f4c82e9012b9406a0218
Luke Rickard <luke.x.rickard@gmail.com>
2017-12-29 16:54:40 +0000

    updating deployment scripts 164

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -euo pipefail
-apt -y install python36
+add-apt-repository -y ppa:jonathonf/python-3.6
+apt-get -y update
+apt-get -y install python3.6
 wget -O /home/ubuntu/deploy/virtualenv-15.1.0.tar.gz https://github.com/pypa/virtualenv/archive/15.1.0.tar.gz
 tar xzvf /home/ubuntu/deploy/virtualenv-15.1.0.tar.gz -C /home/ubuntu/deploy/
-python36 /home/ubuntu/deploy/virtualenv-15.1.0/virtualenv.py /home/ubuntu/deploy/virtual_env
+python3.6 /home/ubuntu/deploy/virtualenv-15.1.0/virtualenv.py /home/ubuntu/deploy/virtual_env
 /home/ubuntu/deploy/virtual_env/bin/pip install -r /home/ubuntu/deploy/requirements.txt
