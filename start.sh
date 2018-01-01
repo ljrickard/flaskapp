6bc034f97c32a25017bbeeadb0c794ed63bdb576
Luke Rickard <luke.x.rickard@gmail.com>
2018-01-01 11:37:17 +0000

    updating deployment scripts 201

--- start.sh
+++ start.sh
@@ -9,9 +9,9 @@ set -euo pipefail
 /home/ubuntu/deploy/flaskapp/virtual_env/bin/celery multi start w1 w2 w3 w4 w5 w6 \
     --app=app.celery \
     --pidfile="/home/ubuntu/deploy/flaskapp/%n.pid" \
-    #--logfile="/home/ubuntu/deploy/flaskapp/logs/celery/%n.log" \
     --workdir="/home/ubuntu/deploy/flaskapp"
     # -Ofair
+    #--logfile="/home/ubuntu/deploy/flaskapp/logs/celery/%n.log" \
 
 systemctl daemon-reload
 systemctl start flower
