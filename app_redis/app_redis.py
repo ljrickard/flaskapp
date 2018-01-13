3b0967abfa2293586871288b27013481f43e55d8
Luke Rickard <luke.x.rickard@gmail.com>
2018-01-13 14:02:01 +0000

    updating deployment scripts 206

--- app_redis/app_redis.py
+++ app_redis/app_redis.py
@@ -12,11 +12,11 @@ class Redis:
 	@classmethod
 	def _create_connection(cls):
 	    try:
-	        redis_connection = redis.StrictRedis(host=cls.URI, 
-	        										port=cls.PORT, 
-	        										db=cls.DB, 
-	        										password=cls.PASSWORD)
-	    	logger.info(redis_connection)
+			redis_connection = redis.StrictRedis(host=cls.URI, 
+													port=cls.PORT, 
+													db=cls.DB, 
+													password=cls.PASSWORD)
+			logger.info(redis_connection)
 	    except redis.ConnectionError:
 	        raise InternalServerError(description='redis ConnectionError')
 	    return redis_connection
