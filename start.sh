c0778e3a20b0d55c7ad8bf5d9ef86bc8ce3bbcba
Luke Rickard <luke.x.rickard@gmail.com>
2018-01-14 12:11:20 +0000

    updating deployment scripts 212

--- start.sh
+++ start.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -euo pipefail
-/home/ubuntu/deploy/flaskapp/virtual_env/bin/gunicorn \
- 	--chdir /home/ubuntu/deploy/flaskapp \
- 	--pid /home/ubuntu/deploy/flaskapp/gunicorn.pid \
- 	--config /home/ubuntu/deploy/flaskapp/gunicorn.py app:app
+#/home/ubuntu/deploy/flaskapp/virtual_env/bin/gunicorn \
+# 	--chdir /home/ubuntu/deploy/flaskapp \
+# 	--pid /home/ubuntu/deploy/flaskapp/gunicorn.pid \
+# 	--config /home/ubuntu/deploy/flaskapp/gunicorn.py app:app
 
 
-/home/ubuntu/deploy/flaskapp/virtual_env/bin/celery multi start w1 w2 w3 w4 w5 w6 \
-    --app=app.celery \
-    --pidfile="/home/ubuntu/deploy/flaskapp/%n.pid" \
-    --logfile="/home/ubuntu/deploy/flaskapp/logs/app/%n.log" \
-    --workdir="/home/ubuntu/deploy/flaskapp"
+#/home/ubuntu/deploy/flaskapp/virtual_env/bin/celery multi start w1 w2 w3 w4 w5 w6 \
+#    --app=app.celery \
+#    --pidfile="/home/ubuntu/deploy/flaskapp/%n.pid" \
+#    --logfile="/home/ubuntu/deploy/flaskapp/logs/app/%n.log" \
+#    --workdir="/home/ubuntu/deploy/flaskapp"
     # -Ofair
 
-systemctl daemon-reload
-systemctl start flower
-systemctl status flower
+#systemctl daemon-reload
+#systemctl start flower
+#systemctl status flower
