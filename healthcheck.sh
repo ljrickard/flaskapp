5013299d19598de9fc9f8784a3f07538faaa1bb5
Luke Rickard <luke.x.rickard@gmail.com>
2018-07-21 17:01:02 +0100

    updating deployment scripts 222

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-http_status=$(curl -sL -w --unix-socket /run/gunicorn/socket "%{http_code}" "http:/healthcheck" -o /dev/null)
+http_status=$(curl --unix-socket /run/gunicorn/socket -sL -w "%{http_code}" "http:/healthcheck" -o /dev/null)
 if [ $http_status = "200" ]; then
    	echo "Endpoint point started successfully"
 else
